0523a1ce6ea55257b411b7389f51d13f5e04c767
Atis <atis@nyantech.com>
2022-04-08 11:35:30 +0900

    Third-Party JavaScript

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>First Post</title>
+        <script src="https://connect.facebook.net/en_US/sdk.js" />
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">
