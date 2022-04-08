d36cc01d15ca49eb851ce252538e798874a95397
Atis <atis@nyantech.com>
2022-04-08 11:43:53 +0900

    Layout Component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from 'next/script';
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         <script src="https://connect.facebook.net/en_US/sdk.js" />
@@ -22,6 +23,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
