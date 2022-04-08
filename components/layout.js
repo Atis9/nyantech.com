689212c434f069e271e68886bf16f2e4b33ce773
Atis <atis@nyantech.com>
2022-04-08 16:11:52 +0900

    fix typo

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link ref="icon" href="favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
