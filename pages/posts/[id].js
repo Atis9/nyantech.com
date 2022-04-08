fc2e51d0f35e80bd5c3c5689f25abba3164c6e77
Atis <atis@nyantech.com>
2022-04-08 15:17:04 +0900

    Implement getStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,16 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
-export default function Post() {
-  return <Layout>...</Layout>
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
 }
 
 export async function getStaticPaths() {
@@ -10,5 +18,14 @@ export async function getStaticPaths() {
   return {
     paths,
     fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
   }
 }
