f2a5f948b7439c589e1a106b9e1912099e75d964
Atis <atis@nyantech.com>
2022-04-09 09:12:03 +0900

    変数の型宣言を追加

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = "Atis"
-export const siteTitle = 'Next.js Sample Website'
+const name: string = "Atis"
+export const siteTitle: string = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
   return (
--- lib/posts.tsx
+++ lib/posts.tsx
@@ -4,7 +4,7 @@ import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
 
-const postsDirectory = path.join(process.cwd(), 'posts')
+const postsDirectory: string = path.join(process.cwd(), 'posts')
 
 interface PostData {
   id: string,
