c0dfae9d5b07325e58a4dff1ff4a0c2847b982ce
Atis <atis@nyantech.com>
2022-04-13 12:22:39 +0900

    Prettier の導入と実行 (#10)
    * yarn add -D prettier eslint-config-prettier
    
    * eslintrc を更新
    
    * echo {}> .prettierrc.json
    
    * prettierrc を更新
    
    * format の script を追加
    
    * yarn format

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,30 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import { ReactNode } from 'react'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import { ReactNode } from 'react';
 
-const name: string = "Atis"
-export const siteTitle: string = 'Next.js Sample Website'
+const name: string = 'Atis';
+export const siteTitle: string = 'Next.js Sample Website';
 
-export default function Layout({ children, home }: { children: ReactNode, home: boolean }) {
+export default function Layout({
+  children,
+  home,
+}: {
+  children: ReactNode;
+  home: boolean;
+}) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="favicon.ico" />
+        <link rel='icon' href='favicon.ico' />
         <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
+          name='description'
+          content='Learn how to build a personal website using Next.js'
         />
         <meta
-          property="og:image"
+          property='og:image'
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
+            siteTitle,
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
 
       <header className={styles.header}>
@@ -32,7 +38,7 @@ export default function Layout({ children, home }: { children: ReactNode, home: 
           <>
             <Image
               priority
-              src="/images/profile.png"
+              src='/images/profile.png'
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -42,11 +48,11 @@ export default function Layout({ children, home }: { children: ReactNode, home: 
           </>
         ) : (
           <>
-            <Link href="/">
+            <Link href='/'>
               <a>
                 <Image
                   priority
-                  src="/images/profile.png"
+                  src='/images/profile.png'
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
@@ -55,7 +61,7 @@ export default function Layout({ children, home }: { children: ReactNode, home: 
               </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
-              <Link href="/">
+              <Link href='/'>
                 <a className={utilStyles.colorInherit}>{name}</a>
               </Link>
             </h2>
@@ -65,11 +71,11 @@ export default function Layout({ children, home }: { children: ReactNode, home: 
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
-          <Link href="/">
+          <Link href='/'>
             <a>← Back to home</a>
           </Link>
         </div>
       )}
     </div>
-  )
+  );
 }
