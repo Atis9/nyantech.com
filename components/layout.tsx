007261831ecdf82d2b27f2d8fb1bf6144ae492ed
Atis <atis@nyantech.com>
2022-04-23 21:31:29 +0900

    試しに og image を削除

--- components/layout.tsx
+++ components/layout.tsx
@@ -21,10 +21,6 @@ export default function Layout({
           name='description'
           content='Nyantech'
         />
-        <meta
-          property='og:image'
-          content='/images/logo.png'
-        />
         <meta name='og:title' content={siteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
