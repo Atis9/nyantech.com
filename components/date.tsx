39ad10f02cd077e30f9c9c2910dccd53d9893516
Atis <atis@nyantech.com>
2022-04-24 10:11:01 +0900

    Date を分まで表示するようにした

--- components/date.tsx
+++ components/date.tsx
@@ -2,5 +2,5 @@ import { parseISO, format } from 'date-fns';
 
 export default function Date({ dateString }: { dateString: string }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'yyyy-MM-dd')}</time>;
+  return <time dateTime={dateString}>{format(date, 'yyyy-MM-dd HH:mm')}</time>;
 }
