7259390c81127f2282f002ad4914533f2fa15b2b
Atis <atis@nyantech.com>
2022-05-01 17:34:49 +0900

    ボーダーを追加

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode } from 'react';
 
-const name: string = 'Atis';
+export const name: string = 'Atis';
 export const siteTitle: string = 'Nyantech';
 
 export default function Layout({
