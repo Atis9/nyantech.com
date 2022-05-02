6b3d08b53ffdc52588c09b77877eb4f056beaebd
Atis <atis@nyantech.com>
2022-05-02 18:18:12 +0900

    フォントに 'BIZ UDPGothic' を指定した (#57)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import '@primer/css/index.scss';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
