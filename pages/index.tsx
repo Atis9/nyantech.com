4e3b29196d01b44a836834d6c313e8ea53d33728
Atis <atis@nyantech.com>
2022-05-02 18:50:31 +0900

    padding を調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,24 +27,24 @@ export default function Home({
       <h1>Blog</h1>
       <div className='Box my-3'>
         <div className='Box-header d-flex'>
-          <div className='flex-1'>Title</div>
+          <div className='flex-1 px-2'>Title</div>
           <div className='flex-1 d-flex'>
-            <div className='flex-1'>Author</div>
-            <div className='flex-1'>Date</div>
+            <div className='flex-1 px-2'>Author</div>
+            <div className='flex-1 px-2'>Date</div>
           </div>
         </div>
         {allPostSummaries.map(({ id, date, title, author }) => (
           <div className='Box-row Box-row--hover-gray d-flex' key={id}>
-            <div className='Box-row-link flex-1'>
+            <div className='Box-row-link flex-1 px-2'>
               <Link href={`/posts/${id}`} passHref>
                 <a><strong>{title}</strong></a>
               </Link>
             </div>
             <div className='flex-1 d-flex'>
-              <div className='flex-1'>
+              <div className='flex-1 px-2'>
                 <strong>{author}</strong>
               </div>
-              <div className='color-fg-muted flex-1'>
+              <div className='color-fg-muted flex-1 px-2'>
                 <Date dateString={date} />
               </div>
             </div>
